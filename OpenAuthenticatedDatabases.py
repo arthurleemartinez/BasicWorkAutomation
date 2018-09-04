caf5ca2c3672d3ad1c5e37ab9a45a473959558bc
Arthur Martinez <40931653+arthurleemartinez@users.noreply.github.com>
2018-09-04 15:31:27 -0500

    Update OpenAuthenticatedDatabases.py

--- OpenAuthenticatedDatabases.py
+++ OpenAuthenticatedDatabases.py
@@ -6,14 +6,14 @@ import webbrowser
 chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
 
 # This URL will be the URL that your login form points to with the "action" tag.
-POST_LOGIN_URL = 'https://www5.elawsoftware.com/eLawSecure.nsf/SecureLogin?ReadForm'
+POST_LOGIN_URL_ELAW = 'https://www5.elawsoftware.com/eLawSecure.nsf/SecureLogin?ReadForm'
 
 # This URL is the page you actually want to pull down with requests.
-REQUEST_URL = 'https://www5.elawsoftware.com/1217/1217_Criminal.nsf/Docket?OpenForm'
+REQUEST_URL_CreateNewFile = 'https://www5.elawsoftware.com/1217/1217_Criminal.nsf/Docket?OpenForm'
 
 values = {'username': 'Leonard.martinez',
-          'password': '516980'}
+          'password': '51698'}
 
-r = requests.post(POST_LOGIN_URL, data=values)
+r = requests.post(POST_LOGIN_URL_ELAW, data=values)
 print(r.content)
-webbrowser.get(chrome_path).open(REQUEST_URL)
+webbrowser.get(chrome_path).open(REQUEST_URL_CreateNewFile)
